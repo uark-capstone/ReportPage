77cc13ec40d43cdbb8c1e9af140e50f384b6d811
Caleb Duke <52674360+Mantleheart@users.noreply.github.com>
2021-02-20 17:09:05 -0600

    Preliminary start of the Reports Page
    The page as of now uses Chart.js and Bootstrap. The focus for this commit was just to get the basic chart working and establish the color scheme.

---
+++ ReportingPage.js
@@ -0,0 +1,33 @@
+﻿
+new Chart(document.getElementById("chart"), {
+    type: 'line',
+    data: {
+        labels: ["9:30 AM", "9:35 AM", "9:40 AM", "9:45 AM", "9:50 AM", "9:55 AM", "10:00 AM"],
+        datasets:
+            [
+                {
+                    data: [12,23,31,41,12,12,9],
+                    label: "Expressions Detected",
+                    borderColor: "#ff602a",
+                    fill: false
+
+                }
+
+            ]
+
+    },
+    options: {
+
+        title:
+        {
+            display: true,
+            text: "Let's see how you did today:"
+
+        }
+
+
+    }
+
+
+
+});
